8d5f4f94898d5f665ed5196b1abc027d98c7909f
nathan-08 <nathanryan001@gmail.com>
2018-04-18 09:56:07 -0600

    json files

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Dashboard from './components/Dashboard/Dashboard'
-import { Button, Panel, PanelGroup } from 'react-bootstrap'
 import Login from './components/Login/Login'
 export default _ => (
     <Switch>
